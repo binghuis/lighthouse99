99398121a473f1df2de999b2cd92fc6a2abffaa2
宋秉徽 <451000936@qq.com>
2024-08-20 10:09:14 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# Lighthouse 满分手册
+# Lighthouse 10 满分手册
 
-本仓库内容是基于 [web.dev 性能优化手册](https://web.dev/learn/performance/welcome?hl=zh-cn) 的实践。
+![googlechrome](https://img.shields.io/badge/Google_Chrome-4285F4?logo=googlechrome&labelColor=263238)
+![lighthouse](https://img.shields.io/badge/Lighthouse-F44B21?logo=lighthouse&labelColor=263238)
 
-1. 如何发现性能问题
-2. 性能问题分类与对应解决方案
-   - 图片、字体等资源问题
-   - JS 执行问题
-   - 渲染问题
-3. 建立长效解决流程
+本仓库内容是基于 [Chrome lighthouse 10 指南](https://developer.chrome.com/docs/lighthouse/performance/performance-scoring?hl=zh-cn) 的实践。
+
+1. 性能优化是系统工程，不是零散的概念罗列。
+2. 性能问题的发现要基于足够的样本数据，而不是开发者的主观感受。
+3. 读懂性能评估依据的主要指标。
+4. 性能指标的上报。
+5. 性能问题分类以及各类问题的优化思路。
+6. 建立现代浏览器的“雅虎军规”。
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,7 @@
-import fs from "fs";
-import lighthouse from "lighthouse";
-import * as chromeLauncher from "chrome-launcher";
+import { onCLS, onFCP, onINP, onLCP, onTTFB } from "web-vitals";
 
-async function main(url: string) {
-  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
-
-  const runnerResult = await lighthouse(url, {
-    logLevel: "info",
-    output: "html",
-    onlyCategories: ["performance"],
-    port: chrome.port,
-  });
-
-  if (!runnerResult) {
-    return;
-  }
-
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-
-  fs.writeFileSync(
-    "lhreport.html",
-    typeof reportHtml === "string" ? reportHtml : reportHtml.join("\n")
-  );
-
-  // `.lhr` is the Lighthouse Result as a JS object
-  console.log(`网站 ${runnerResult.lhr.finalDisplayedUrl} 性能测试完成`);
-
-  if (
-    !runnerResult.lhr.categories["performance"] ||
-    !runnerResult.lhr.categories["performance"].score
-  ) {
-    return;
-  }
-
-  const score = runnerResult.lhr.categories["performance"].score * 100;
-
-  console.log(`性能分数: ${score}`);
-
-  await chrome.kill();
-}
-
-main("https://better-use-of-react.vercel.app/").catch(console.error);
+onCLS(console.log);
+onFCP(console.log);
+onINP(console.log);
+onLCP(console.log);
+onTTFB(console.log);
