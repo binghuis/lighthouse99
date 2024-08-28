0893a8f5117a37d814de70a0130dbcc5f3ea2dcd
宋秉徽 <451000936@qq.com>
2024-08-28 16:06:31 +0800

    feat(lh99): 添加将消息添加到队列的功能（addToQueue）和在页面隐藏后将队列刷新的功能（flushQueue）

--- packages/lh99/src/index.ts
+++ packages/lh99/src/index.ts
@@ -1,26 +1,48 @@
 import { onCLS, onFCP, onINP, onLCP, onTTFB, Metric } from "web-vitals";
 // import {onLCP, onINP, onCLS} from 'web-vitals/attribution';
 
-function sendToAnalytics(metric: Metric) {
-  console.log(metric);
+const queue = new Set();
 
-  const body = JSON.stringify(metric);
+function addToQueue(metric: Metric) {
+  queue.add(metric);
+}
 
-  // navigator.sendBeacon("/analytics", body);
+function flushQueue() {
+  if (queue.size > 0) {
+    // Replace with whatever serialization method you prefer.
+    // Note: JSON.stringify will likely include more data than you need.
+    const body = JSON.stringify([...queue]);
+
+    // if (navigator.sendBeacon) {
+    //   navigator.sendBeacon("/analytics", body);
+    // } else {
+    //   fetch("/analytics", { body, method: "POST", keepalive: true });
+    // }
+
+    queue.clear();
+  }
 }
 
 export class LH99 {
   appId: string;
+
   constructor(appId: string) {
     this.appId = appId;
+    addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "hidden") {
+        flushQueue();
+      }
+    });
   }
+
   init() {
-    if (typeof window !== "undefined") {
-      onFCP(sendToAnalytics);
-      onCLS(sendToAnalytics);
-      onINP(sendToAnalytics);
-      onLCP(sendToAnalytics);
-      onTTFB(sendToAnalytics);
+    if (typeof window === "undefined") {
+      return;
     }
+    onFCP(addToQueue);
+    onLCP(addToQueue);
+    onCLS(addToQueue);
+    onINP(addToQueue);
+    onTTFB(addToQueue);
   }
 }
